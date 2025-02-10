e16b6846a91e6506b7b8c1942f259f70535aef8b
amolsingh <amolsingh9372@gmail.com>
2025-02-09 20:20:05 -0500

    first commit

---
+++ src/main/java/com/events/planner/gateway/EventsPlannerGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.events.planner.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventsPlannerGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventsPlannerGatewayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/events/planner/gateway/EventsPlannerGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.events.planner.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventsPlannerGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
